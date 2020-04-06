3add1a43046affe2acf0c329bd2a1e899a86b754
sratta <sratta@amazon.com>
2020-04-06 17:16:09 -0500

    Working auth functionality

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@
  * @flow strict-local
  */
 
-import React from 'react';
+import React, { Component } from 'react';
 import {
   SafeAreaView,
   StyleSheet,
@@ -24,12 +24,24 @@ import {
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
 import { Vitals } from './screens/Vitals';
-import Amplify from 'aws-amplify';
+import Amplify, { Auth } from 'aws-amplify';
 import awsconfig from './aws-exports';
-import { withAuthenticator } from 'aws-amplify-react-native';
+import { withAuthenticator, AmplifyAuthenticator, AmplifySignOut, SignUp } from 'aws-amplify-react-native';
 Amplify.configure(awsconfig);
 
-const App: () => React$Node = () => {
+ const App: () => React$Node = () => {
+//const App = () => {
+  // signUp = async () => {
+  //   const {username, password,email, phone_number} = this.state
+  //   try {
+  //     await Auth.signUp({username,password,attributes:{email, phone_number}})
+  //     console.log('Signed up!')
+  //     this.setState({stage:1})
+  //   }
+  //   catch(err) {
+  //     console.log('error signing up',err)
+  //   }
+  // }
   return (
     <>
       <StatusBar barStyle="dark-content" />
@@ -38,74 +50,7 @@ const App: () => React$Node = () => {
   );
 };
 
-const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
-});
-
-const signUpConfig = {
-  header: 'Sign Up',
-  hideAllDefaults: false,
-  defaultCountryCode: '1',
-  signUpFields: [
-    {
-      label: 'Username',
-      key: 'Username',
-      required: true,
-      displayOrder: 1,
-      type: 'string'
-    },
-    {
-      label: 'Email',
-      key: 'email',
-      required: true,
-      displayOrder: 2,
-      type: 'string'
-    },
-    {
-      label: 'Password',
-      key: 'password',
-      required: true,
-      displayOrder: 3,
-      type: 'password'
-    }
-  ]
-};
-
 // Comment this out to get an un autheniticated Apps
-// export default withAuthenticator(App, { signUpConfig });
-export default App;
+//export default withAuthenticator(App, { signUpConfig });
+export default withAuthenticator(App, true);
+//export default App;
